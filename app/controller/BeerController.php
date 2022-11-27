593af03315dd3e0622c41cb2314a79a1a8f735f7
Jeremiah <jtowe1@asu.edu>
2022-11-26 19:46:39 -0500

    Create dao for testing

--- app/controller/BeerController.php
+++ app/controller/BeerController.php
@@ -2,6 +2,7 @@
 
 namespace BTL\Controller;
 
+use BTL\Model\Beer\Dao;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -9,6 +10,7 @@ class BeerController
 {
     public function list(Request $request, Response $response, array $args): Response
     {
+        $beerDao = new Dao();
         $body = $response->getBody();
         $body->write(json_encode(['test' => 'value']));
         return $response->withHeader('Content-Type', 'application/json');
