6cb88889e9b9bae09a3b8ea12f222b5162fce06d
Jeremiah <jtowe1@asu.edu>
2022-11-27 17:32:31 -0500

    Add getters to beer

--- app/model/Beer.php
+++ app/model/Beer.php
@@ -7,12 +7,34 @@ use Carbon\CarbonImmutable;
 class Beer
 {
     public function __construct(
-        private int $id,
+        private ?int $id,
         private string $name,
         private int $styleId,
         private CarbonImmutable $created,
         private CarbonImmutable $lastUpdated
     ) {
 
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getStyleId(): int
+    {
+        return $this->styleId;
     }
 }
