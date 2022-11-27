6c2d988d217f44d5cc35adcfabe42b2e3a6b3823
Jeremiah <jtowe1@asu.edu>
2022-11-27 17:32:59 -0500

    Get all by offset in beer controller list

--- app/controller/BeerController.php
+++ app/controller/BeerController.php
@@ -8,11 +8,14 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 class BeerController
 {
-    public function list(Request $request, Response $response, array $args): Response
+    public function list(Request $request, Response $response): Response
     {
+        $offset = $request->getQueryParams()['offset'] ?? 0;
+
         $beerDao = new Dao();
+        $beers = $beerDao->getAllStartingAt($offset);
         $body = $response->getBody();
-        $body->write(json_encode(['test' => 'value']));
+        $body->write(json_encode($beers));
         return $response->withHeader('Content-Type', 'application/json');
     }
 }
