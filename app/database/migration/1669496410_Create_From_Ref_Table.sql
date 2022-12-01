8f84f32d46de22ac8647fdb3da4bb509c3aaa4d7
Jeremiah <jtowe1@asu.edu>
2022-12-01 18:45:54 -0500

    Add beer.from

---
+++ app/database/migration/1669496410_Create_From_Ref_Table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE from (
+    id INT auto_increment PRIMARY KEY,
+    from VARCHAR(255)
+);
--- app/database/migration/1669496415_Create_Beer_Table.sql
+++ app/database/migration/1669496415_Create_Beer_Table.sql
@@ -2,7 +2,9 @@ create table beer (
     id int auto_increment primary key,
     name varchar(255),
     style_id int,
+    from_id int,
     created datetime,
     last_updated datetime,
-    FOREIGN KEY (style_id) REFERENCES style(id)
+    FOREIGN KEY (style_id) REFERENCES style(id),
+    FOREIGN KEY (from_id) REFERENCES ref_from(id)
 );
